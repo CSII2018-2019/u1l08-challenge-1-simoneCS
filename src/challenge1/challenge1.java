a439636f3612fa3cdc1d1110d285d98c4368b669
rothsteins <rothsteins@LR064W78-1718.ellis.int>
2018-10-24 20:58:01 -0400

    Actual Final Code

--- src/challenge1/challenge1.java
+++ src/challenge1/challenge1.java
@@ -3,13 +3,12 @@ package challenge1;
 public class challenge1 {
 
 	public challenge1() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		double [] array = {1,7,4,6,3,9,2,8};
 
-		int savingVariable = 0;
+		int saveVariable = 0;
 
 		
 
